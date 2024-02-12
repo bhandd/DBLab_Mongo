c943818abd1967f29d824ea72129e2a7fa397a1a
bjorn <bjorn1987@gmail.com>
2024-02-12 11:56:21 +0100

    added module mongodb.sync. updated module-info.java and pom.xml

--- src/main/java/dblab/dblab_mongo/Main.java
+++ src/main/java/dblab/dblab_mongo/Main.java
@@ -1,11 +1,18 @@
 package dblab.dblab_mongo;
 
+import com.mongodb.ClientSessionOptions;
+import com.mongodb.connection.ClusterDescription;
 import dblab.dblab_mongo.model.BooksDb;
 import dblab.dblab_mongo.model.BooksDbInterface;
 import dblab.dblab_mongo.view.BooksPaneView;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import com.mongodb.client.*;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import java.util.List;
 
 
 //import dbgui.lab1examplegui.model.BooksDbMockImpl;
@@ -53,7 +60,104 @@ public class Main extends Application {
         super.stop();
     }
     public static void main(String[] args) {
-        launch(args);
+        MongoClient mongoClient = new MongoClient() {
+            @Override
+            public MongoDatabase getDatabase(String s) {
+                return null;
+            }
+
+            @Override
+            public ClientSession startSession() {
+                return null;
+            }
+
+            @Override
+            public ClientSession startSession(ClientSessionOptions clientSessionOptions) {
+                return null;
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public MongoIterable<String> listDatabaseNames() {
+                return null;
+            }
+
+            @Override
+            public MongoIterable<String> listDatabaseNames(ClientSession clientSession) {
+                return null;
+            }
+
+            @Override
+            public ListDatabasesIterable<Document> listDatabases() {
+                return null;
+            }
+
+            @Override
+            public ListDatabasesIterable<Document> listDatabases(ClientSession clientSession) {
+                return null;
+            }
+
+            @Override
+            public <TResult> ListDatabasesIterable<TResult> listDatabases(Class<TResult> aClass) {
+                return null;
+            }
+
+            @Override
+            public <TResult> ListDatabasesIterable<TResult> listDatabases(ClientSession clientSession, Class<TResult> aClass) {
+                return null;
+            }
+
+            @Override
+            public ChangeStreamIterable<Document> watch() {
+                return null;
+            }
+
+            @Override
+            public <TResult> ChangeStreamIterable<TResult> watch(Class<TResult> aClass) {
+                return null;
+            }
+
+            @Override
+            public ChangeStreamIterable<Document> watch(List<? extends Bson> list) {
+                return null;
+            }
+
+            @Override
+            public <TResult> ChangeStreamIterable<TResult> watch(List<? extends Bson> list, Class<TResult> aClass) {
+                return null;
+            }
+
+            @Override
+            public ChangeStreamIterable<Document> watch(ClientSession clientSession) {
+                return null;
+            }
+
+            @Override
+            public <TResult> ChangeStreamIterable<TResult> watch(ClientSession clientSession, Class<TResult> aClass) {
+                return null;
+            }
+
+            @Override
+            public ChangeStreamIterable<Document> watch(ClientSession clientSession, List<? extends Bson> list) {
+                return null;
+            }
+
+            @Override
+            public <TResult> ChangeStreamIterable<TResult> watch(ClientSession clientSession, List<? extends Bson> list, Class<TResult> aClass) {
+                return null;
+            }
+
+            @Override
+            public ClusterDescription getClusterDescription() {
+                return null;
+            }
+        }
+
+//        launch(args);
 //        Author author1 = new Author(1, "kalle balle");
 //        Author author2 = new Author(2, "gurra murra");
 //        Author author3 = new Author(2, "gert Stjhärt");
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,11 @@
 module dblab.dblab_mongo {
+
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires org.mongodb.driver.sync.client;
+    requires org.mongodb.driver.core;
+    requires org.mongodb.bson;
 
 
     opens dblab.dblab_mongo to javafx.fxml;
